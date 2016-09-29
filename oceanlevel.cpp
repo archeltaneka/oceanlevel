5d7b817e2ce5056d73eb8bb8532d14da19f00e20
Archel Taneka <archeltaneka@ymail.com>
2016-09-29 16:43:18 +0700

    first commit

---
+++ oceanlevel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// declare variables of ocean level and increment each year
+	float levelRising =	1.5;
+	float oceanLevel = 0.0;
+	int years;
+	
+	// increment of 1.5 each year on ocean level and print out
+	for(years = 1; years <= 25; years++)
+	{
+		oceanLevel = oceanLevel + levelRising;
+		cout << years << " year(s)" << " ocean level: " << oceanLevel << endl;
+	}
+	
+}
